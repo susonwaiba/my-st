af7efc52cb59ca95f90af05880eb60143f1e5b27
Suson Waiba <susonwaiba@gmail.com>
2020-11-14 18:57:26 +0545

    updates while monochrome

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Cascadia Code PL:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "Cascadia Code PL:pixelsize=16:antialias=true:autohint=true" };
+static char *font2[] = { "Font Awesome 5 Free Regular:pixelsize=16:antialias=true:autohint=true", "Font Awesome 5 Free Solid:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
